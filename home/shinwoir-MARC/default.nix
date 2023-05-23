faabf939c79b1ce8c47705efb967fffec5112647
Raphael Leroy <raphael.leroy@epita.fr>
2023-05-23 17:57:10 +0200

    ansible + bump flake lock

--- home/shinwoir-MARC/default.nix
+++ home/shinwoir-MARC/default.nix
@@ -13,6 +13,10 @@
   home.homeDirectory = lib.mkForce "/Users/shinwoir/";
   security.yubikey.enable = true;
 
+  home.packages = with pkgs; [
+    ansible
+  ];
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards

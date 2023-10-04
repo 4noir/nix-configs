13ec2f294dd4b87e131876d57cbb26b98ca2c1f9
4noir <4noir@protonmail.com>
2023-10-04 10:01:55 +0200

    bitwig

--- modules/music/bitwig.nix
+++ modules/music/bitwig.nix
@@ -9,27 +9,24 @@
         url = "https://downloads.bitwig.com/stable/${finalAttrs.version}/${prevAttrs.pname}-${finalAttrs.version}.deb";
         sha256 = "sha256-IkhUkKO+Ay1WceZNekII6aHLOmgcgGfx0hGo5ldFE5Y=";
       };
-      buildInputs = prevAttrs.buildInputs ++ (with pkgs; [ gnupg ]);
-      installphase = ''
-        runhook preinstall
+      buildInputs = prevAttrs.buildInputs ++ (with pkgs; [ gnupg tree ]);
+      installPhase = ''
+        runHook preInstall
+
+        rm opt/bitwig-studio/bin/bitwig.jar
+        cp ${./bitwig.jar} opt/bitwig-studio/bin/bitwig.jar
+        cp ${./bitwig.jar} opt/bitwig-studio/bin/bitwig-cracked.jar
 
         mkdir -p $out/bin
-
-
-        # rm opt/bitwig-studio/bin/
         cp -r opt/bitwig-studio $out/libexec
-        # gpg -d ${./bitwig.jar.gpg}
-        cp bitwig.jar $out/libexec
         ln -s $out/libexec/bitwig-studio $out/bin/bitwig-studio
         cp -r usr/share $out/share
-
-
-        substitute usr/share/applications/com.bitwig.bitwigstudio.desktop \
-          $out/share/applications/com.bitwig.bitwigstudio.desktop \
+        substitute usr/share/applications/com.bitwig.BitwigStudio.desktop \
+          $out/share/applications/com.bitwig.BitwigStudio.desktop \
           --replace /usr/bin/bitwig-studio $out/bin/bitwig-studio
 
-          runhook postinstall
-      '';
+        runHook postInstall
+        '';
     }))
   ];
 }
